a35e0230a4eea45b38a95b23ddaee8931bf7d3d8
SquidRings1 <146839825+SquidRings1@users.noreply.github.com>
2024-12-03 08:31:05 +0100

    fix dump issue because no sleep

--- src/main/java/fr/lernejo/logger/ConsoleLogger.java
+++ src/main/java/fr/lernejo/logger/ConsoleLogger.java
@@ -1,8 +1,7 @@
 package fr.lernejo.logger;
 
-import fr.lernejo.logger.Logger;
-
 public class ConsoleLogger implements Logger {
+
     @Override
     public void log(String message) {
         System.out.println(message);
--- src/main/java/fr/lernejo/logger/Logger.java
+++ src/main/java/fr/lernejo/logger/Logger.java
@@ -2,10 +2,4 @@ package fr.lernejo.logger;
 
 public interface Logger {
     void log(String message);
-
-    class LoggerFactory {
-        public static Logger getLogger(String name) {
-            return new ConsoleLogger();
-        }
-    }
 }
